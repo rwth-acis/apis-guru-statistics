60861a68e1290703cb3e257384de5e36c6369aa0
Dominik Kus <dominik@kdtk.de>
2019-08-28 17:13:19 +0200

    Update downloaded data from APIs.Guru and Requirements-Bazaar

--- src/downloader/requirementsbazaar-downloader.js
+++ src/downloader/requirementsbazaar-downloader.js
@@ -8,6 +8,10 @@ const rp = require('request-promise-native');
 const sw2oai = require('swagger2openapi');
 const fse = require('fs-extra');
 const path = require('path');
+const addLinkDefinitions = require('openapi-link-generator').default;
+const { createGraphQlSchema } = require('openapi-to-graphql');
+const { printSchema } = require('graphql');
+
 const directory = path.resolve(__dirname, '../../data/requirements-bazaar');
 const apiUrl = 'https://requirements-bazaar.org/bazaar/swagger.json';
 
@@ -21,7 +25,11 @@ async function loadData() {
 
   const swaggerFile = path.join(directory, 'swagger.json');
   const openapiFile = path.join(directory, 'openapi.json');
+  const openapiLinksFile = path.join(directory, 'openapi-links.json');
+  const gqlFile = path.join(directory, 'graphql-nolinks.graphql');
+  const gqlLinksFile = path.join(directory, 'graphql-links.graphql');
 
+  // Download the original Swagger 2.0 file
   let swaggerText;
   try {
     swaggerText = await rp(apiUrl);
@@ -31,6 +39,7 @@ async function loadData() {
     return;
   }
 
+  // Convert it to OpenAPI 3.0
   let openapiObj;
   try {
     openapiObj = await sw2oai.convertStr(swaggerText, { patch: true, direct: true });
@@ -40,9 +49,33 @@ async function loadData() {
     return;
   }
 
+  // Add links using the link-generator
+  let openapiWithLinks;
+  try {
+    openapiWithLinks = addLinkDefinitions(openapiObj).oas;
+  } catch (err) {
+    console.error('Failed to add link definitions to OpenAPI documentation.');
+    console.error(err.message);
+    return;
+  }
+
+  // Generate two GraphQL schemas. One for the OpenAPI with links and one for the OpenAPI without links.
+  let gqlWithoutLinks, gqlWithLinks;
+  try {
+    gqlWithoutLinks = (await createGraphQlSchema(openapiObj)).schema;
+    gqlWithLinks = (await createGraphQlSchema(openapiWithLinks)).schema;
+  } catch (err) {
+    console.error('Failed to generate the GraphQL schemas.');
+    console.error(err.message);
+    return;
+  }
+
   try {
     await fse.writeFile(swaggerFile, swaggerText, { flag: 'w' });
     await fse.writeFile(openapiFile, JSON.stringify(openapiObj), { flag: 'w' });
+    await fse.writeFile(openapiLinksFile, JSON.stringify(openapiWithLinks), { flag: 'w' });
+    await fse.writeFile(gqlFile, printSchema(gqlWithoutLinks), { flag: 'w' });
+    await fse.writeFile(gqlLinksFile, printSchema(gqlWithLinks), { flag: 'w' });
   } catch (err) {
     console.error('Failed to write files.');
     console.error(err.message);
